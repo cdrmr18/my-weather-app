a509b6e93ee77caba111ee2d6de8ffdeb6af4c57
Kathy Tavia <kathy.tavia@gmail.com>
2020-11-08 01:05:10 -0800

    Worked on setting up 3-hour forecasts

--- script.js
+++ script.js
@@ -53,7 +53,48 @@ function search(city) {
   let apiKey = "ce96567bcfe36200b8c50bb6f61e4a04";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
   axios.get(apiUrl).then(showWeather);
+
+  apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=${units}`
+  axios.get(apiUrl).then(displayForcast)
 }
+
+function formatHours(timestamp) {
+  let now = new Date();
+  let hour = now.getHours();
+  if (hour < 10) {
+    hour = `0${hour}`;
+  }
+  let minutes = now.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${hour}:${minutes}`
+}
+
+  function displayForcast(response) {
+    let forecastElement = document.querySelector("#forecast");
+    forecastElement.innerHTML = null;
+    let forecast = null;
+
+    for (let index = 0; index < 5; index++) {
+      forecast = response.data.list[index];
+      forecastElement.innerHTML += `
+        <div class="col-2 forecast-time">
+            <h3>
+              ${formatHours(forecast.dt * 1000)}
+            </h3>
+            <img 
+              src= "http://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png"
+              alt=""
+            />
+            <div class="forecast-temp">
+              <strong>${Math.round(forecast.main.temp_max)}°</strong> ${Math.round(forecast.main.temp_min)}°
+            </div>
+          </div>
+        `;
+    }
+  }
+
 function showWeather(response) {
   document.querySelector("#city-name").innerHTML = response.data.name;
   document.querySelector("#current-temp").innerHTML = Math.round(
