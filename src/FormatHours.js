f9a05cc9d7d763780be0e35d91c3ec3e565fd14f
Kathy Tavia <kathy.tavia@gmail.com>
2020-11-22 12:30:40 -0800

    fixed style

---
+++ src/FormatHours.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+export default function FormatHours(timestamp) {
+  let date = new Date(timestamp);
+  let hour = date.getHours();
+  if (hour < 10) {
+    hour = `0${hour}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${hour}:${minutes}`
+}
